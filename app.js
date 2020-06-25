b54b84c28220465c7f0b7bb222ab246c048a6236
tim hopp <timothyjhopp@gmail.com>
2020-06-25 00:20:11 -0600

    improved readability

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ let game = {
 
 function play(playersChosen) {
   computerRandom()
-  if(playersChosen == 'rock'){
-    game.playerChoice = 'rock';
-  } else if (playersChosen == 'paper'){
-    game.playerChoice = 'paper'
-  } else if (playersChosen == 'scissors'){
-    game.playerChoice = 'scissors';
+
+  switch(playersChosen){
+    case 'rock': game.playerChoice = 'rock'
+    break;
+    case 'paper': game.playerChoice = 'paper'
+    break;
+    case 'scissors': game.playerChoice = 'scissors'
+    break;
   }
+ 
   game.computerChoice = `${game.computerChoice}`;
   playLogic();
 }
@@ -20,13 +23,14 @@ function play(playersChosen) {
 
 function computerRandom() {
   let computerhasChose = Math.floor(Math.random() * 4)
-  if (computerhasChose == 3){
-    game.computerChoice = "paper";
-  } else if ( computerhasChose == 2){
-    game.computerChoice = "scissors";
-  } else if (computerhasChose == 1){
-    game.computerChoice = "rock";
-   }
+switch(computerhasChose){
+  case 1: game.computerChoice = "rock" 
+  break;
+  case 2: game.computerChoice = "scissors"
+  break;
+  case 3: game.computerChoice = "paper"
+  break; 
+ }
 }
 
 //*Add the ability for the player and computer choice to change within play 
@@ -51,10 +55,10 @@ let compPick = game.computerChoice;
 
 
 
+
 function draw(){
 let userImg = document.getElementById("user-img");
 let compImg = document.getElementById("computer-img")
-
 
 if(game.playerChoice == 'rock'){
   userImg.src = "icons8-rock-80.png"
@@ -70,5 +74,5 @@ if(game.computerChoice == 'rock'){
   compImg.src= "icons8-barber-scissors-80.png";
 } else if (game.computerChoice =='paper'){
   compImg.src = "icons8-paper-plane-64.png";
-}
+ }
 }
